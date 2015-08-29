39590f0a25444e758782c6b44562b43e22391308
James Bennett <J.bennett@gmx.com>
2015-08-28 17:25:16 -0700

    adds live blogs from parse.com database

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # blog
 my blog site!
+<p>V 0.0.3 - populates blogs from database on parse.com
 <p>V 0.0.1 - Creates layout, template using bootstraps blog template.  
--- js/blog.js
+++ js/blog.js
@@ -1,13 +1,29 @@
 $(function() {
 
     Parse.$ = jQuery;
- 
+
     Parse.initialize("Ea78s9QiTCskbiLZirAyhq591UjlQ6vXJYNm81G9", "CFwJjWiD3P7MBZbRYQiTNofDoa5VJVzp9YRlog67");
 
-    var TestObject = Parse.Object.extend("TestObject");
-    var testObject = new TestObject();
-    testObject.save({foo: "bar"}).then(function(object) {
-      alert("yay! it worked");
-    });
-
+  var Blog      = Parse.Object.extend("Blog");
+  var Blogs     = Parse.Collection.extend({
+      model: Blog
+  });
+  var blogs  = new Blogs();
+  blogs.fetch({
+    success: function(blogs) {
+      var blogsView = new BlogsView({ collection: blogs });
+      blogsView.render();
+      $('.main-container').html(blogsView.el);
+    },
+    error: function(blogs, error) {
+      console.log(error);
+    }
+  });
+  var BlogsView = Parse.View.extend({
+    template: Handlebars.compile($('#blogs-tpl').html()),
+    render: function(){
+      var collection = { blog: this.collection.toJSON() };
+      this.$el.html(this.template(collection));
+    }
+  });
 });
