ad30ffe9c54ac85bfdc8b5c985f6219cbdc44a98
James Bennett <J.bennett@gmx.com>
2015-09-03 16:07:50 -0700

    Adds blog list view with links for edit/delete. Introduces router for reduced redundancy.

--- js/admin.js
+++ js/admin.js
@@ -23,9 +23,10 @@ $(function() {
             Parse.User.logIn(username, password, {
               // If the username and password matches
               success: function(user) {
-                var welcomeView = new WelcomeView({model: user});
-                welcomeView.render();
-                $('.main-container').html(welcomeView.el);
+                // var welcomeView = new WelcomeView({model: user});
+                // welcomeView.render();
+                // $('.main-container').html(welcomeView.el);
+                blogRouter.navigate('admin', {trigger: true });
               },
               // If there is an error
               error: function(user, error) {
@@ -44,31 +45,32 @@ $(function() {
             'click .add-blog': 'add'
           },
           add: function(){
-            var addBlogView = new AddBlogView();
-            addBlogView.render();
-            $('.main-container').html(addBlogView.el);
+            blogRouter.navigate('add', {trigger: true});
+          //   var addBlogView = new AddBlogView();
+          //   addBlogView.render();
+          //   $('.main-container').html(addBlogView.el);
           },
           render: function(){
             var attributes = this.model.toJSON();
             this.$el.html(this.template(attributes));
-            this.$el.html(this.template()).find('textarea').wysihtml5();
-            var blogs = new Blogs();
-            blogs.fetch({
-              success: function(blogs) {
-                var blogsAdminView = new BlogsAdminView({ collection: blogs });
-                blogsAdminView.render();
-                $('.main-container').append(blogsAdminView.el);
-              },
-              error: function(blogs, error) {
-                console.log(error);
-              }
-            });
+            // this.$el.html(this.template()).find('textarea').wysihtml5();
+            // var blogs = new Blogs();
+            // blogs.fetch({
+            //   success: function(blogs) {
+            //     var blogsAdminView = new BlogsAdminView({ collection: blogs });
+            //     blogsAdminView.render();
+            //     $('.main-container').append(blogsAdminView.el);
+            //   },
+            //   error: function(blogs, error) {
+            //     console.log(error);
+            //   }
+            // });
           }
         });
 
-        var loginView = new LoginView();
-        loginView.render();
-        $('.main-container').html(loginView.el);
+        // var loginView = new LoginView();
+        // loginView.render();
+        // $('.main-container').html(loginView.el);
 
         var AddBlogView = Parse.View.extend({
           template: Handlebars.compile($('#add-tpl').html()),
@@ -120,7 +122,70 @@ $(function() {
             this.$el.html(this.template(collection));
           }
         });
+        var BlogRouter = Parse.Router.extend({
 
+          //Define shared vairables
+
+          initialize: function(options){
+            this.blogs = new Blogs();
+          },
+
+          add: function(){
+            var addBlogView = new AddBlogView();
+            addBlogView.render();
+            $('.main-container').html(addBlogView.el);
+          },
+
+          //Start Router
+          start: function(){
+            Parse.history.start({pushState: true});
+            this.navigate('admin', {trigger: true });
+          },
+
+          //map functions to urls
+          //just add '{{URL pattern=}}' : '{{functoin name}}'
+          routes:{
+            'admin'    : 'admin',
+            'login'    : 'login',
+            'add'      : 'add',
+            'edit/:url': 'edit'
+          },
+
+          admin: function() {
+            var currentUser = Parse.User.current();
+
+            if (!currentUser) {
+              blogRouter.navigate('login', {trigger: true});
+            } else {
+
+              var welcomeView = new WelcomeView({model: currentUser});
+              welcomeView.render();
+              $('.main-container').html(welcomeView.el);
+
+              this.blogs.fetch({
+                success: function(blogs) {
+                  var blogsAdminView = new BlogsAdminView({ collection: blogs});
+                  blogsAdminView.render();
+                  $('.main-container').append(blogsAdminView.el);
+                },
+                error: function(blogs, error) {
+                  console.log(error);
+                }
+              });
+            }
+          },
+          login: function() {
+            var loginView = new LoginView();
+            loginView.render();
+            $('.main-container').html(loginView.el);
+          },
+          // add  : function() {},
+          edit : function(url) {},
+
+        }),
+        blogRouter = new BlogRouter();
+
+      blogRouter.start();
 
 
 });
